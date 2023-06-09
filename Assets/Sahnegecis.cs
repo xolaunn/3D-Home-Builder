e1bf22f5d1c27bc3ee313de7af6370f5f12ed371
Yasir Bülbül <yasirblbl0325@gmail.com>
2023-06-09 23:31:48 +0300

    Artırılmış Gerçeklik Destekli 3D Yapı Modelleme Uygulaması
    Artırılmış Gerçeklik Destekli 3D Yapı Modelleme Uygulaması

---
+++ Assets/Sahnegecis.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Sahnegecis : MonoBehaviour
+
+{
+	public void Sahnedegistir(int sahne_id) {
+		SceneManager.LoadScene(sahne_id);
+	}
+}
